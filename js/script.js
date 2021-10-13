289a47af9c6c55fc77fe6abbcc33f14af736d8ff
tolusolaadeyemi <tolusa@hotmail.com>
2021-10-13 22:35:42 +0100

    first draft

---
+++ js/script.js
@@ -0,0 +1,98 @@
+const mobileNavLinks = document.querySelectorAll('.mobile-nav-link'),
+  navLinks = document.querySelectorAll('.nav-link'),
+  sections = document.querySelectorAll('section'),
+  header = document.querySelector('header'),
+  form = document.querySelector('form'),
+  loader = document.querySelector('.loader'),
+  name = document.querySelector('#name'),
+  email = document.querySelector('#email'),
+  subject = document.querySelector('#subject'),
+  message = document.querySelector('#message'),
+  containAll = document.querySelector('.contain-all'),
+  navigationCheckbox = document.querySelector('.navigation-checkbox'),
+  aboutCards = document.querySelectorAll('.about-page-card'),
+  certificateShow = document.querySelector('.certificate-img-contain'),
+  certificateShowImg = document.querySelector('.certificate-img'),
+  viewCertificate = document.querySelector('.view-certificate'),
+  certificateBtns = document.querySelectorAll('.certificate-btn');
+
+
+const resizeHeight = () => {
+  aboutCards.forEach(aboutCard => {
+    aboutCard.style.height = getComputedStyle(aboutCard).width;
+  });
+}
+resizeHeight();
+window.onresize = function() {
+          aboutCards.forEach(aboutCard => {
+            aboutCard.style.height = getComputedStyle(aboutCard).width;
+          });
+}
+
+const isInViewport = (el) => {
+  const rect = el.getBoundingClientRect();
+  // let elId = el.id;
+  // console.log(rect.top);
+  // console.log(rect.bottom);
+  // console.log(elId);
+  if (
+    // rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.top <= 100
+    // rect.top <= document.documentElement.scrollTop || document.body.scrollTop
+
+  ) {
+    navLinks.forEach((navLink) => {
+      navLink.classList.contains(el.id) ? navLink.classList.add('active') : navLink.classList.remove('active');
+    })
+  }
+}
+navLinks.forEach(navLink => {
+  navLink.addEventListener('click', () => {
+    navigationCheckbox.checked = false;   
+  })
+})
+
+document.addEventListener('scroll', () => {
+  sections.forEach((section) => {
+    isInViewport(section);
+  })
+});
+
+// Show error message
+const showError = (message) => {
+  const formGroup = email.parentElement.closest('.input-wrap');
+  formGroup.querySelector('small').innerText = message;
+};
+const saveMessage = () => {
+  let newMessageRef = messagesRef.push();
+   newMessageRef.set({
+     name: name.value,
+     email: email.value,
+     subject: subject.value,
+     message: message.value,
+   });
+};
+
+function validateEmail(emailValid) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  let emailValidTest = re.test(String(emailValid).toLowerCase());
+  if (emailValidTest === true) {
+    saveMessage();
+    showError('');
+  email.style.marginBottom = "12px";
+  form.querySelectorAll('input').forEach(inputTag => {
+    inputTag.value = '';
+  });
+  form.querySelector('textarea').value = '';
+} else {
+  showError('Email invalid');
+  email.style.marginBottom = "0px";
+  }
+}
+
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+  if (email.value !== '') {
+    validateEmail(email.value);
+  }
+});
